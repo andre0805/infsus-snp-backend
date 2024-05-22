de68e74fe8f2327c00d218bc0ab36c2f9a597bb0
Andre Flego <ade.flego@gmail.com>
2024-05-22 20:56:01 +0200

    init commit

---
+++ src/main/java/com/fer/snp/backend/SnpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fer.snp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fer/snp/backend/SnpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fer.snp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
