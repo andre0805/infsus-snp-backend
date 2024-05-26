970fdbd763dc170a95f351cc30749907357f973c
Andre Flego <ade.flego@gmail.com>
2024-05-26 21:31:02 +0200

    refactor Status id

--- src/main/java/com/fer/snp/backend/entities/Termin.java
+++ src/main/java/com/fer/snp/backend/entities/Termin.java
@@ -17,7 +17,6 @@ import java.time.LocalDateTime;
 @Table(name = "termin")
 public class Termin {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDateTime dateTime;
